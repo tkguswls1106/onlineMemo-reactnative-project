9d3ea7892d451a59880d0b5eb19a9aeb0e831010
sahyunjin <tkguswls1106@naver.com>
2023-07-25 21:35:48 +0900

    리액트 네이티브와 expo로 모바일 웹뷰앱 구현 성공. 일단은 안드로이드 위주로 확인 완료하였음. 커밋.

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import { useEffect, useRef } from 'react';
+import { Text, TextInput } from 'react-native';
+import { BackHandler } from 'react-native';
 
 export default function App() {
+
+  const webview = useRef(null);
+  const onAndroidBackPress = () => {
+    if (webview.current) {
+      webview.current.goBack();
+      return true; // prevent default behavior (exit app)
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    // Text 적용
+    Text.defaultProps = Text.defaultProps || {};
+    Text.defaultProps.allowFontScaling = false;
+
+    // TextInput 적용
+    TextInput.defaultProps = TextInput.defaultProps || {};
+    TextInput.defaultProps.allowFontScaling = false;
+
+    BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
+    };
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      style={styles.container}
+      source={{ uri: 'https://www.onlinememo.kr' }}
+      ref={webview}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
   },
 });
